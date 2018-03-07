d2aeb5fefb7e3b98299c663bf9359d3885b37eb8
pradyumna <newton@newtonflash.com>
2018-03-07 19:50:56 +0530

    added missing directory

--- src/components/product.js
+++ src/components/product.js
@@ -1,16 +1,14 @@
 import React, { Component, Fragment } from 'react';
 import style from './product.css';
+import PropTypes from 'prop-types';
+
 
 class Product extends Component {
-
-
     render() {
         let data = this.props.data;
         return (
             <div className={style.product}>
-                <div>
-                    <image src="" />
-                </div>
+
                 <h2>{data.name}</h2>
                 <p>{data.desc}</p>
                 <div>{data.price}</div>
@@ -22,6 +20,15 @@ class Product extends Component {
         );
     }
 }
+
+Product.propTypes = {
+    data: PropTypes.shape({
+        name: PropTypes.string.isRequired,
+        desc: PropTypes.string,
+        price: PropTypes.any
+    })
+};
+
 
 export default Product;
 
